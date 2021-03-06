48cb10959c93813e79ee299e7e1ecedd4635ca8b
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-03-05 19:19:23 -0800

    Imported insertSeedData method and added async-await function to grab it onConnect when command run.

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -8,6 +8,7 @@ import {
   withItemData,
   statelessSessions,
 } from '@keystone-next/keystone/session';
+import { insertSeedData } from './seed-data';
 
 const databaseURL = process.env.DATABASE_URL;
 
@@ -37,7 +38,12 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
-      // TODO: Add data seeding here
+      async onConnect(keystone) {
+        console.log('Connected to the Database');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       // Schema items go in here
