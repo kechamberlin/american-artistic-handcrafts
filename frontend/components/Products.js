1380bff6efb6e93c19d74a550b10754bfe7085cb
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-03-08 12:47:43 -0800

    Created ProductsListStyles variables to display products as a grid.

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -20,6 +20,12 @@ const ALL_PRODUCTS_QUERY = gql`
   }
 `;
 
+const ProductsListStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+`;
+
 export default function Products() {
   const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
   console.log(data, error, loading);
