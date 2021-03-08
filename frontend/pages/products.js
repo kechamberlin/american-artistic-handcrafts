bea0b85355ed0792ca1a98a9140e1f6978655e26
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-03-08 12:06:57 -0800

    Imported Products Component and added to render.

--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,3 +1,9 @@
+import Products from '../components/Products';
+
 export default function ProductsPage() {
-  return <div>Products Page</div>;
+  return (
+    <div>
+      <Products />
+    </div>
+  );
 }
