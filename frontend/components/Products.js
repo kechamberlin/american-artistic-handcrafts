d1d2b46071391e22ac396c1ccc54b5406260e3a0
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-03-08 12:06:44 -0800

    Created a Products Component with a GraphQL query and basic mapping of the products.

---
+++ frontend/components/Products.js
@@ -0,0 +1,36 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const ALL_PRODUCTS_QUERY = gql`
+  query ALL_PRODUCTS_QUERY {
+    allProducts {
+      id
+      name
+      price
+      description
+      photo {
+        id
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+export default function Products() {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+  console.log(data, error, loading);
+  // TODO: Add a loading spinner animation
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  return (
+    <div>
+      <div>
+        {data.allProducts.map((product) => (
+          <p key={product.id}>{product.name}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
