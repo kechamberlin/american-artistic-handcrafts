0e2ed5310d06d07853124a539ad3ee0668fe15b7
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-03-09 09:32:33 -0800

    Created useForm hook with handleChange, resetForm, and clearForm functions.

---
+++ frontend/lib/useForm.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+export default function useForm(initial = {}) {
+  // Create a state object for our inputs
+  const [inputs, setInputs] = useState(initial);
+
+  function handleChange(e) {
+    let { value, name, type } = e.target;
+    // Keeps change in number value as a number intead of converting it to a string
+    if (type === 'number') {
+      value = parseInt(value);
+    }
+    if (type === 'file') {
+      value[0] = e.target.files;
+    }
+
+    // Do it this way when you have an object with multiple pieces of state
+    setInputs({
+      // Copy the existing state
+      ...inputs,
+      [name]: value,
+    });
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key, value]) => [key, ''])
+    );
+    setInputs(blankState);
+  }
+
+  // Return the things we want to surface from this custom hook
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm,
+  };
+}
