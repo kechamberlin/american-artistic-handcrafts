0abf292040443097d43cfa2c1341f33e420186ab
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-03-09 15:02:30 -0800

    Created a DeleteProduct Component with delete product mutation and renders a button with onClick handler to delete the product.

---
+++ frontend/components/DeleteProduct.js
@@ -0,0 +1,34 @@
+// PLAY WITH DIFFERENT WAYS TO CONFIRM DELETING ITEM (i.e., a pop-up modal, etc)
+// PLAY WITH DIFFERENT KINDS OF ALERTS (i.e., toasters)
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+export default function DeleteProduct({ id, children }) {
+  const [deleteProduct, { loading }] = useMutation(DELETE_PRODUCT_MUTATION, {
+    variables: { id: id },
+  });
+  return (
+    <button
+      type="button"
+      disabled={loading}
+      onClick={() => {
+        if (confirm('Are you sure you want to delete this item?')) {
+          // Go ahead and delete
+          console.log('Deleting Item...');
+          deleteProduct().catch((err) => alert(err.message));
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
+}
