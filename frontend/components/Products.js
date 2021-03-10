02c01ab185d45bace187a4efde706ad8b5897244
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-03-10 12:44:24 -0800

    Added skip, first variables to the query to access them.

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -5,8 +5,8 @@ import { perPage } from '../config';
 import Product from './Product';
 
 export const ALL_PRODUCTS_QUERY = gql`
-  query ALL_PRODUCTS_QUERY {
-    allProducts {
+  query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
+    allProducts(first: $first, skip: $skip) {
       id
       name
       price
