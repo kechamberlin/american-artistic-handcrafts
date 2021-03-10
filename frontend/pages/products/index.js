7dbc3940ead58feede603f7f682b7aff2041e261
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-03-10 11:22:08 -0800

    Imported and made variable using useRouter.

--- frontend/pages/products/index.js
+++ frontend/pages/products/index.js
@@ -1,7 +1,9 @@
+import { useRouter } from 'next/dist/client/router';
 import Pagination from '../../components/Pagination';
 import Products from '../../components/Products';
 
 export default function ProductsPage() {
+  const { query } = useRouter();
   return (
     <div>
       <Pagination page={1} />
