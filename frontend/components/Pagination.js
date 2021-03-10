8a122b70d302df4c6f57f11983ab5a5634b97fcd
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-03-10 10:48:06 -0800

    Created pageCount variable to calculate total number of pages.

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -16,11 +16,11 @@ const PAGINATION_QUERY = gql`
 
 export default function Pagination({ page }) {
   const { error, loading, data } = useQuery(PAGINATION_QUERY);
-
   if (loading) return 'Loading...';
   if (error) return <DisplayError error={error} />;
-
   const { count } = data._allProductsMeta;
+  const pageCount = Math.ceil(count / perPage);
+
   return (
     <PaginationStyles>
       <Head>
