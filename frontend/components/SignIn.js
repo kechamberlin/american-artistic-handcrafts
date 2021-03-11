b022a181715a55bc7eba6eb341030c76559f4b54
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-03-11 10:58:17 -0800

    Created a SignIn Component with Form styles and labels for the email and password.

---
+++ frontend/components/SignIn.js
@@ -0,0 +1,33 @@
+import Form from './styles/Form';
+
+export default function SignIn() {
+  return (
+    <Form method="POST">
+      <fieldset>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email Address"
+            autoComplete="email"
+            // value
+            // onChange
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            autoComplete="password"
+            // value
+            // onChange
+          />
+        </label>
+        <button type="submit">Sign In</button>
+      </fieldset>
+    </Form>
+  );
+}
