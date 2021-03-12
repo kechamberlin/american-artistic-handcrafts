1834f78e9c05669dba609ce18f38d4ba1d9366b2
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-03-12 13:26:00 -0800

    Added button that uses openCart helper function.

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -16,6 +16,9 @@ export default function Nav() {
           <Link href="/orders">Orders</Link>
           <Link href="/account">Account</Link>
           <SignOut />
+          <button type="button" onClick={openCart}>
+            My Cart
+          </button>
         </>
       )}
       {!user && (
