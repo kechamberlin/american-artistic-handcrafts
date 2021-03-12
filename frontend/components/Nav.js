426d5b31a6ed390c98509324da3a57db4caae078
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-03-12 13:25:37 -0800

    Imported useCart hook and added to Nav function; deleted console log.

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,11 +1,12 @@
 import Link from 'next/link';
+import { useCart } from '../lib/cartState';
 import SignOut from './SignOut';
 import NavStyles from './styles/NavStyles';
 import { useUser } from './User';
 
 export default function Nav() {
   const user = useUser();
-  // console.log(user);
+  const { openCart } = useCart();
   return (
     <NavStyles>
       <Link href="/products">Products</Link>
