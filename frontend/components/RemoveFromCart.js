a8cf6dc023151ef9334a5d55fec6dafb741d328f
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-03-12 15:38:36 -0800

    Created update function to evict cart item from cache.

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -20,6 +20,10 @@ const REMOVE_FROM_CART_MUTATION = gql`
   }
 `;
 
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteCartItem));
+}
+
 export default function RemoveFromCart({ id }) {
   const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
     variables: {
