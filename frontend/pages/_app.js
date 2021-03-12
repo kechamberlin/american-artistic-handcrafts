1d2af9624db24a945bc013d07e81bf23d2cc4b86
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-03-12 13:05:32 -0800

    Imported and rendered CartStateProvider (wraps around Page Component).

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,6 +4,7 @@ import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
 import '../components/styles/nprogress.css';
 import withData from '../lib/withData';
+import { CartStateProvider } from '../lib/cartState';
 
 // CHANGE NProgress CSS
 
@@ -14,9 +15,11 @@ Router.events.on('routeChangeError', () => NProgress.done());
 function MyApp({ Component, pageProps, apollo }) {
   return (
     <ApolloProvider client={apollo}>
-      <Page>
-        <Component {...pageProps} />
-      </Page>
+      <CartStateProvider>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </CartStateProvider>
     </ApolloProvider>
   );
 }
