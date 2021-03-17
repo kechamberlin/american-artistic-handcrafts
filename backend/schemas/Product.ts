2a2a354887949108ccaca9eb03403edcf4200636
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-03-17 10:59:11 -0700

    Imported isSignedIn, rules from access and wrote appropriate access points for each.

--- backend/schemas/Product.ts
+++ backend/schemas/Product.ts
@@ -1,9 +1,14 @@
 import { integer, relationship, select, text } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
+import { isSignedIn, rules } from '../access';
 
 export const Product = list({
-  // TODO
-  // access:
+  access: {
+    create: isSignedIn,
+    read: rules.canReadProducts,
+    update: rules.canManageProducts,
+    delete: rules.canManageProducts,
+  },
   fields: {
     name: text({ isRequired: true }),
     description: text({
--- backend/schemas/User.ts
+++ backend/schemas/User.ts
@@ -1,9 +1,20 @@
 import { list } from '@keystone-next/keystone/schema';
 import { text, password, relationship } from '@keystone-next/fields';
+import { permissions, rules } from '../access';
 
 export const User = list({
-  // access:
-  // ui
+  access: {
+    create: () => true,
+    read: rules.canManageUsers,
+    update: rules.canManageUsers,
+    // Only people with the permission can delete themselves; You can't delete yourself
+    delete: permissions.canManageUsers,
+  },
+  ui: {
+    // Hide backend UI from regular users
+    hideCreate: (args) => !permissions.canManageUsers(args),
+    hideDelete: (args) => !permissions.canManageUsers(args),
+  },
   fields: {
     name: text({ isRequired: true }),
     email: text({ isRequired: true, isUnique: true }),
